a565c04ff443d043af1a236474cdcdfb6c4e662b
cmd-kvn <kvn.m.wong@gmail.com>
2016-11-13 13:18:10 -0800

    finished problem4

--- app.js
+++ app.js
@@ -99,12 +99,23 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+// the first element is the sum of the numbers in the array,
+// numbers = testArray[0]...testArray[2]
+// sum function returns array, value is stored at first element
+  var sumIndex0Index1 = sum(testArray[0], testArray[1]); //returns array, value is stored at first element
+  var sumToArray = sum(sumIndex0Index1[0], testArray[2]); //returns array, value is stored at first element
+  console.log('sumIndex0Index1 array: ', sumIndex0Index1);
+  console.log('sumToArray array:', sumToArray);
+// the second element is a string = '2,3,4 was passed in as an array of numbers, and 9 is their sum.'
+  var returnIndex1Element =  testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumToArray[0] + ' is their sum.';
+  console.log('returnIndex1Element is: ', returnIndex1Element);
+// returns an array [sum, string]
+  return [sumToArray[0], returnIndex1Element];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -115,7 +126,7 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 "The numbers 2,3,4 have a product of 24."
 
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-
+=
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
